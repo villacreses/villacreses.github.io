849277dfc5b2d88e18c7305f16c58343f9d13dd8
Mario Villacreses <mario@doc.ai>
2020-04-18 19:10:15 -0700

    Refactor SkillLogos component to make use of graphQL

--- src/components/skill-icons.js
+++ src/components/skill-icons.js
@@ -1,108 +1,39 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
 const query = graphql`
   query {
-    js: file(relativePath: { eq: "javascript.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    node: file(relativePath: { eq: "node.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    html_css: file(relativePath: { eq: "html-html5-css-css3.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    sass: file(relativePath: { eq: "sass.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    react: file(relativePath: { eq: "react.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    redux: file(relativePath: { eq: "redux.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    git: file(relativePath: { eq: "git.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    sql: file(relativePath: { eq: "sql.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    postgres: file(relativePath: { eq: "postgres.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-
-    mongo: file(relativePath: { eq: "mongo.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 300) {
-          ...GatsbyImageSharpFluid
+    allSkillLogosJson {
+      nodes {
+        title
+        slug
+        image {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
   }
 `;
 
-const SkillLogos = () => (
-  <StaticQuery
-    query={query}
-    render={data => (
-      <div className="skill-logo-container">
-        {Object.entries(data).map(
-          ([ key, { childImageSharp: { fluid } } ]) => (
-            <Img
-              key={key}
-              className={`skill-logo ${key}`}
-              fluid={fluid}
-            />
-          )
-        )}
-      </div>
-    )}
-  />
-);
+const SkillLogos = () => {
+  const data = useStaticQuery(query);
+  return (
+    <div className="skill-logo-container">
+      {data.allSkillLogosJson.nodes.map(({ image, slug, ...imageProps }) => (
+        <Image
+          {...imageProps}
+          key={slug}
+          className={`skill-logo ${imageProps.key}`}
+          fluid={image.childImageSharp.fluid}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default SkillLogos;
