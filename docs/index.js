b1a9d56b014c401ee06eb06cf406df18ef34d586
Mario Villacreses <MarioVillacreses@outlook.com>
2025-05-25 21:45:35 -0700

    Tab navigation

--- docs/index.js
+++ docs/index.js
@@ -2,15 +2,34 @@ import { DarkToggleActions } from "./assets/components/mv-dark-toggle.js";
 
 function onDOMLoaded() {
   DarkToggleActions.register()
-  const menuToggle = document.getElementById('menu-toggle');
+  handleAccessibilityOfClickables();
 
   window.addEventListener("pageshow", () => {
     DarkToggleActions.onPageShow();
 
+    const menuToggle = document.getElementById('menu-toggle');
     if (menuToggle) {
       menuToggle.checked = false;
     }
   });
+}
+
+function handleAccessibilityOfClickables() {
+  // For debugging:
+  // document.addEventListener("focusin", (event) => {
+  //   console.log("Focused element:", event.target);
+  // });
+
+  Array.from(document.querySelectorAll('#nav-actions label'))
+    .forEach(toggle => toggle.addEventListener("keydown", function (event) {
+      if (event.key === " " || event.key === "Enter") {
+        event.preventDefault(); // Prevent scrolling
+        const checkbox = document.getElementById(toggle.getAttribute('for'));
+        checkbox.checked = !checkbox.checked;
+      }
+    }));
+
+  const menuToggle = document.getElementById('menu-toggle');
 
   menuToggle.addEventListener('change', function () {
     const selector = [
@@ -28,7 +47,13 @@ function onDOMLoaded() {
     ['main', '.home-link', 'footer'].forEach(selector => {
       document.querySelector(selector).setAttribute('aria-hidden', this.checked);
     })
-  })
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if ((event.key === "Escape" || event.key === "Esc") && menuToggle.checked) {
+      menuToggle.checked = false;
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', onDOMLoaded);
