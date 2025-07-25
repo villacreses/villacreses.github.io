9952aca4cda613d8f22df888eb3d7d44135a7cab
Mario Villacreses <MarioVillacreses@outlook.com>
2025-07-24 22:46:32 -0400

    Add styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 import markdownIt from "markdown-it";
 import yaml from "js-yaml";
+import * as sass from "sass";
+import path from "node:path";
 
 const md = new markdownIt();
 
@@ -7,12 +9,33 @@ export default async function(eleventyConfig) {
   eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
   eleventyConfig.addDataExtension("yml", contents => yaml.load(contents)); 
 
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // Output as .css
+    useLayouts: false, // Disable layout processing
+
+    compile: async function (inputContent, inputPath) {
+      const parsed = path.parse(inputPath);
+      if (parsed.name.startsWith("_")) return;
+
+      const result = sass.compileString(inputContent, {
+        loadPaths: [parsed.dir || ".", this.config.dir.includes],
+      });
+
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async () => result.css;
+    },
+  });
+
   eleventyConfig.addNunjucksFilter("markdown", function(content) {
     return md.render(content);
   });
 
+  eleventyConfig.addPassthroughCopy("index.css");
+  eleventyConfig.addPassthroughCopy("assets/styles/**/*.css");
+
   return {
     htmlTemplateEngine: "njk",
-    templateFormats: ["html", "njk", "md"]
+    templateFormats: ["html", "njk", "md", "scss"]
   };
 };
