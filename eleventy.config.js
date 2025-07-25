846796c4ebdaf2caa16236a2ce9a157464adddaa
Mario Villacreses <MarioVillacreses@outlook.com>
2025-07-24 21:18:31 -0400

    Add career page content

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,18 @@
 import markdownIt from "markdown-it";
+import yaml from "js-yaml";
 
 const md = new markdownIt();
 
 export default async function(eleventyConfig) {
-	// Configure Eleventy
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents)); 
+
   eleventyConfig.addNunjucksFilter("markdown", function(content) {
     return md.render(content);
   });
+
+  return {
+    htmlTemplateEngine: "njk",
+    templateFormats: ["html", "njk", "md"]
+  };
 };
