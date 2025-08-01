1eef04e49ec5f4d3221b70a3b3a1feb6a2174064
Mario Villacreses <MarioVillacreses@outlook.com>
2025-08-01 19:15:13 -0400

    Move homepage content to `_data`

--- _data/site.js
+++ _data/site.js
@@ -1,4 +1,5 @@
 export default {
   title: "Mario Villacreses",
   description: "Personal website of Mario Villacreses. Software engineer, math enthusiast, and accessibility-first builder.",
+  location: "New York City",
 }
